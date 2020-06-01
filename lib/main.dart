5564f10da6eac160965eb8562a8cb440834fc4d3
robt <dev.craftmyit.co.uk>
2020-06-01 10:32:26 +0100

    ex005_Function calls

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,62 @@
 
-
-void main() {
-   // opt 4 - print the output /  (return statement ) from a function 
-  print (msg4());
+// ex005_Function calls -------------------------
+// below are 5 variations on simple dart code incorprating variable initialisation and assignment.
+// The code also demonstates basic function declartion and calling options
+//
+// opt 1 ----------
+// Title: Print a String literal
+// call the [ print ] function passing in a <string literal> 'hello world' 
+// Code -------------
+// void main(){
+//   print ('helo world');
+// }
   
-  
-}
+// opt 2 ----------
+// Brief: Print a String Variable
+// call the [ print ] function passing in a <string variable> 'hello world';
+// Code -------------
+// void main(){
+//  String msg = 'hello world';
+//  print (msg);
+// }
 
-String msg4(){
-  var message = 'Simple message to the console - using a [ return ] from a function call';
-  return message;
+// opt 3 ----------
+// Brief: Print a String literal from a function  
+// Code Description: call a function [ msg() ]. The function has an expression that calls the [ print() ] function.
+// Pass into the [ print() ] function the string literal [ 'hello' ]
+// Code ----------
+// void main() {
+//  msg();
+// }
+// msg(){
+//  print ('Hello World');
+// }
+
+// Opt 4 ----------
+// Brief: Pass a String literal to a function that prints the string.
+// Code Description: Pass a <string literal> to a function [ msg() ]. [ print() ] the string literal from within the functions <expression> 
+// Code ----------
+// void main(){
+//  msg('Hello World');
+// }
+// msg(msgx){
+//   print (msgx);
+// }
+
+// Opt 5 --------
+// Brief: 
+// Print a [ return ] string from a function
+// Code Description: 
+// Create a function [ msg() ].  Within the ( expression ) 
+// of the function [ return ] a ( String Literal ) [ 'Hello World' ]
+// Code ----------
+// void main(){
+//  print (msg());
+// }
+// msg(){
+//   return 'Hello World';
+// }
+
+void main(){
+  
 }
