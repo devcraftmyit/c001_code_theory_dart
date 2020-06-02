33eca30f39997ab258133145c55d2609a326bfa2
robt <dev.craftmyit.co.uk>
2020-06-02 09:54:56 +0100

    13. Creating Class Instances

--- lib/main.dart
+++ lib/main.dart
@@ -1,194 +1,198 @@
 
-
-
-
-
-
-// Code Theory: 5. Our First Program -------------------------
-
-
-
-
-// Introduction
-
-// below are 5 variations on simple dart code incorprating variable initialisation and assignment.
-// The code also demonstates basic function declartion and calling options
-
-// opt 1 ----------
-// Title: Print a String literal
-// call the [ print ] function passing in a <string literal> 'hello world' 
-// Code -------------
-// void main(){
-//   print ('helo world');
-// }
-  
-// opt 2 ----------
-// Brief: Print a String Variable
-// call the [ print ] function passing in a <string variable> 'hello world';
-// Code -------------
-// void main(){
-//  String msg = 'hello world';
-//  print (msg);
-// }
-
-// opt 3 ----------
-// Brief: Print a String literal from a function  
-// Code Description: call a function [ msg() ]. The function has an expression that calls the [ print() ] function.
-// Pass into the [ print() ] function the string literal [ 'hello' ]
-// Code ----------
-// void main() {
-//  msg();
-// }
-// msg(){
-//  print ('Hello World');
-// }
-
-// Opt 4 ----------
-// Brief: Pass a String literal to a function that prints the string.
-// Code Description: Pass a <string literal> to a function [ msg() ]. [ print() ] the string literal from within the functions <expression> 
-// Code ----------
-// void main(){
-//  msg('Hello World');
-// }
-// msg(msgx){
-//   print (msgx);
-// }
-
-// Opt 5 --------
-// Brief: 
-// Print a [ return ] string from a function
-// Code Description: 
-// Create a function [ msg() ].  Within the ( expression ) 
-// of the function [ return ] a ( String Literal ) [ 'Hello World' ]
-// Code ----------
-// void main(){
-//  print (msg());
-// }
-// msg(){
-//   return 'Hello World';
-// }
-
-
-
-
-
-// Code Theory: 7. Functions in Dart
-
-
-
-
-// Introduction
-
-// These code tests deal with an example of function annotation
-// 
-
-// Code Test 1 ----------
-// Title: String Type Functions
-// Code Description:
-// Declare a function [ msg(){} ] of type [ String ]  
-// Code Answer -------------
-//void main(){
-//  int x = 2;
-//  int z;
-//  String y;
-//  if (x == 1){ 
-//      y = 'Hello World';
-//      msg(y);
-//      } else if (x == 2){
-//        z = 1343344;
-//        msg(z);
-//      }
-//}
-
-//dynamic msg(y){
-// print (y);
-//}
-
-
-
-
-
-// Code Theory: 10. String Interpolation
-
-
-
-
-// Introduction
-
-// These code tests deal with examples of String Interpolation
-// 
-
-// Template - Code Test
-// Code Test 1 ----------
-// Title: Strings containing [ double ] variables
-// Code Description:
-// declare a variable [ pie ] of type [ double  ]. Assign the value [ 3.124 ]  to [ pie ]  
-// Use the [ print ] function with a string literal argument ' The value of pie to 3 decimals is '
-// Append the [ double ] variable [ pie ] to the string literal using the appropiate notation.
-// Use 2nd [ print ] statement to print the string as above and the length of the value of [ pie ] 
-// Code Answer -------------
-//void main(){
-//  double pie = 3.142;
-//  String pieString = '3.142'; 
-  
-//  print ('The value of pie to 3 decimals is $pie');
-//  print ('The value of pie to 3 decimals is $pie then length of pie is ${pieString.length} ');
-//}
-
-
-
-
+/* -----------------------------------------------------------
+  Code Theory: 5. Our First Program 
+  -----------------------------------------------------------
+  Introduction
  
-// Code Theory: N. xxxxxx
-
-
-
-
-// Introduction
-
-// These code tests deal with examples of function annotation
-// 
-
-// Template - Code Test
-// Code Test 1 ----------
-// Title: String Type Functions
-// Code Description:
-// Declare a function [ msg(){} ] of type [ String ]  
-// Code Answer -------------
-void main(){
+  below are 5 variations on simple dart code incorprating variable initialisation and assignment.
+  The code also demonstates basic function declartion and calling options
+ 
+  opt 1 ----------
+  Title: Print a String literal
+  call the [ print ] function passing in a <string literal> 'hello world' 
+  Code -------------
+*/
+ /*
+  void main(){
+    print ('helo world');
+  }
+*/  
+/*
+ opt 2 ----------
+  Brief: Print a String Variable
+  call the [ print ] function passing in a <string variable> 'hello world';
+  Code -------------
+*/
+ /*
+  void main(){
+   String msg = 'hello world';
+   print (msg);
+  }
+*/
+/*
+ opt 3 ----------
+  Brief: Print a String literal from a function  
+  Code Description: call a function [ msg() ]. The function has an expression that calls the [ print() ] function.
+  Pass into the [ print() ] function the string literal [ 'hello' ]
+  Code ----------
+*/
+ /*
+  void main() {
+   msg();
+  }
+  msg(){
+   print ('Hello World');
+  }
+*/ 
+/*
+Opt 4 ----------
+  Brief: Pass a String literal to a function that prints the string.
+  Code Description: Pass a <string literal> to a function [ msg() ]. [ print() ] the string literal from within the functions <expression> 
+  Code ----------
+*/
+ /*  
+  void main(){
+   msg('Hello World');
+  }
+  msg(msgx){
+    print (msgx);
+  }
+*/ 
+/*
+Opt 5 --------
+  Brief: 
+  Print a [ return ] string from a function
+  Code Description: 
+  Create a function [ msg() ].  Within the ( expression ) 
+  of the function [ return ] a ( String Literal ) [ 'Hello World' ]
+  Code ----------
+*/
+ /*
+  void main(){
+   print (msg());
+  }
+  msg(){
+    return 'Hello World';
+  }
+*/
+/*
+  -----------------------------------------------------------
+  Code Theory: 7. Functions in Dart
+  -----------------------------------------------------------
+ 
+  Introduction
+ 
+  These code tests deal with an example of function annotation
+  
+  Code Test 1 ----------
+  Title: String Type Functions
+  Code Description:
+  Declare a function [ msg(){} ] of type [ String ]  
+  Code Answer -------------
+ */
+ /* 
+  void main(){
+   int x = 2;
+   int z;
+   String y;
+   if (x == 1){ 
+       y = 'Hello World';
+       msg(y);
+       } else if (x == 2){
+         z = 1343344;
+         msg(z);
+       }
+  }
+  dynamic msg(y){
+    print (y);
+  }
+*/ 
+/*
+  -----------------------------------------------------------
+  Code Theory: 10. String Interpolation
+  -----------------------------------------------------------
+ 
+  Introduction
+ 
+  These code tests deal with examples of String Interpolation
+  
+  Code Test 1 ----------
+  Title: Strings containing [ double ] variables
+  Code Description:
+  declare a variable [ pie ] of type [ double  ]. Assign the value [ 3.124 ]  to [ pie ]  
+  Use the [ print ] function with a string literal argument ' The value of pie to 3 decimals is '
+  Append the [ double ] variable [ pie ] to the string literal using the appropiate notation.
+  Use 2nd [ print ] statement to print the string as above and the length of the value of [ pie ] 
+  Code Answer -------------
+*/
+/*
+  void main(){
+   double pie = 3.142;
+   String pieString = '3.142'; 
+  
+   print ('The value of pie to 3 decimals is $pie');
+   print ('The value of pie to 3 decimals is $pie then length of pie is ${pieString.length} ');
+  }
+*/ 
+/*
+  -----------------------------------------------------------
+  Code Theory: 13. Creating Class Instances
+  -----------------------------------------------------------
+ 
+  Introduction
+ 
+  These code tests deal with examples defining classes
+  
+  Template - Code Test
+  Code Test 1 ----------
+  Title: Class instantiation
+  Code Description: ----------
+  call a class method that prints a name 
+  Class [ Person ] 
+  Create a class called the [ Person ] class, declare a [ String ] variable 
+  [ firstName ]  create a method called [ printName ] that prints the [ firstName ]
+ 
+  Function [ Main ]
+  create the dart launch function [ main ]
+  declare an instance [ person ]  of the class [ Person ]  in the [ main(){} ] function body
+  declare the variable [ firstName ] in the [ main ] function and assign the string value ' Rob Tweedie '
+  call the method [ printName() ] setting the argument as [ firstName ] 
+  Code Answer -------------
+*/
+/*  void main(){
+  final person = Person();
+  String firstName = 'Robert Tweedie' ;
+  person.printName(firstName);
  }
 
-// Templates ----------------------------
-
-// Template - Exercise header 
-// Code Theory: N. xxxxxx
-
-
-
-
-// Introduction
-
-// These code tests deal with examples of function annotation
-// 
-
-// Template - Code Test
-// Code Test 1 ----------
-// Title: String Type Functions
-// Code Description:
-// Declare a function [ msg(){} ] of type [ String ]  
-// Code Answer -------------
+ class Person {
+   String firstName = '';
+   printName(firstName){
+     print (firstName);
+   }
+ } 
+ */
+ /*
+  Template
+ 
+  -----------------------------------------------------------
+  Code Theory: nn. xxxx
+  -----------------------------------------------------------
+ 
+  Introduction
+ 
+  These code tests deal with examples defining classes
+  
+  Template - Code Test
+  Code Test 1 ----------
+  Title: Class instantiation
+  Code Description: ----------
+  xxxxxxxxxxxxxxxx
+  Code Answer -------------
+*/
+ /*
 void main(){
+  
  }
-
-
-
-
-
-
-
-
-
-
-
-
-
+ */
+ /*
